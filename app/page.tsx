c98634901a43bddf3f7f3e3c00cbc203570cac95
Sté[PH]ane <steph.guery@gmail.com>
2025-05-06 14:48:00 +0000

    ShadcnUI-Ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { Card } from "@/components/ui/card";
-import Image from "next/image";
-
 export default function Home() {
-  return <div><Card /></div>
+  return <div>Hello world !</div>;
 }
